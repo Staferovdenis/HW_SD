538688f5a7d1d17fc901219faca0368ed7ca2327
Staferovdenis <staferovdenis7@gmail.com>
2020-03-29 19:17:13 +0300

    Task 3_1

---
+++ SoftServe task 3_1/src/com/company/Dog.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Dog {
+    private String name;
+    private int age;
+    private String breed;
+
+    public Dog(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+enum breed{
+    Terrier,Basenji,Beagle;
+
+}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return name.equals(dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, breed);
+    }
+}
---
+++ SoftServe task 3_1/src/com/company/Main.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Dog Terrier = new Dog("Tery",4,"Terrier");
+        Dog dog = new Dog("Tery",5,"Dog");
+        Dog jax = new Dog("Jax",9,"Beagle");
+        System.out.println(dog.getBreed());
+        System.out.println(chekName(Terrier,dog));
+
+
+
+        /*System.out.println("Enter you error code");
+        switch (sc.nextInt()) {
+            case 400 :
+                HTTPError httpError400 = HTTPError.valueOf("BAD_REQUEST");
+                System.out.println(httpError400);
+                break;
+            case 401:
+                HTTPError httpError401 = HTTPError.valueOf("Unauthorized");
+                System.out.println(httpError401);
+                break;
+            case 402 :
+                HTTPError httpError402 = HTTPError.valueOf("Payment_Required");
+                System.out.println(httpError402);
+                break;
+        }
+        float number1 = sc.nextFloat();
+        float number2 = sc.nextFloat();
+        float number3 = sc.nextFloat();
+
+        if(number1 > -5.0 && number1 < 5.0 && number2 > -5.0 && number2 < 5.0&&number3 > -5.0 && number3 < 5.0){
+            System.out.println("Good");
+        }else System.out.println("Badly");
+        System.out.println();
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+
+        int max = MIN_VALUE;
+        int min = MAX_VALUE;
+        if (n1 > max) max = n1;
+        if (n2 > max) max = n2;
+        if (n3 > max) max = n3;
+
+        if (n1 < min) min = n1;
+        if (n2 < min) min = n3;
+        if (n3 < min) min = n3;
+
+        System.out.println("Max = " + max + "; " + "Min = " + min);*/
+    }
+    public static boolean chekName(Object a,Object b){
+        return a.equals(b);
+    }
+
+   public enum HTTPError{
+        BAD_REQUEST(400),Unauthorized(401),Payment_Required(402);
+       private final int namberEr;
+
+       HTTPError(int namber){
+           this.namberEr = namber;
+       }
+
+       public int getNamberEr() {
+           return namberEr;
+       }
+   }
+}
